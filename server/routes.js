0602835f0c54a5899ea0f6e459d1c606f978ea01
Tom Coughlin <tcoughlin3@gmail.com>
2016-03-10 23:30:59 -0600

    (chore) Add framework of server routes.

--- server/routes.js
+++ server/routes.js
@@ -9,19 +9,54 @@ var env = require('node-env-file');
 if (process.env.NODE_ENV !== 'production') {
   env(path.resolve('./.env'));
 }
-
+x
 var jwtCheck = expressjwt({
   secret: new Buffer(process.env.AUTH_SECRET, 'base64'),
   audience: process.env.AUTH_ID
 });
 
-var authRoutes = [];
+var authRoutes = ['/users', '/users/friends', '/games', 'games/invites', '/score/total'];
 
 var routes = [
   {
     path: '/',
     get: function (req, res) {
       res.sendFile(public + '/index.html');
+    }
+  },
+  {
+    path: '/users',
+    post: function (req, res) {
+      // Store new user data in db.
+    }
+  },
+  {
+    path: '/users/friends',
+    get: function (req, res) {
+      // Query db for all of user's friends and their game stats
+      // to display rankings, etc
+    }
+  },
+  {
+    path: '/games',
+    get: function (req, res) {
+      // Query db for all user's games.
+      // Can be used for displaying total number of games played on profile, etc.
+    }
+  },
+  {
+    path: 'games/invites',
+    get: function (req, res) {
+      // Query db for pending game invitations
+      // (Possible) Add invite_status column to users_games table to indicate if 
+      // its a game for which the invitation is pending, accepted or declined.
+    }
+  },
+  {
+    path: '/score/total', 
+    get: function (req, res) {
+      // Query db for all user_game scores in order
+      // to return a total score to display on a profile, etc.
     }
   },
   {
