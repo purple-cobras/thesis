dfd9831605f9a8e52134c36e8a0e01c8279c0c2f
Benjamin Baum <bjb253@cornell.edu>
2016-03-11 12:32:05 -0600

    (chore) add invite column to users_games

--- db/knexfile.js
+++ db/knexfile.js
@@ -3,7 +3,7 @@ var env = require('node-env-file');
 var path = require('path');
 
 if (process.env.NODE_ENV !== 'production') {
-  env(path.resolve('.env'));
+  env('../.env');
 }
 
 var knex = Knex({
@@ -15,7 +15,7 @@ var knex = Knex({
     database: 'thesis',
     charset: 'utf8'
   },
-  migrations {
-    directory: path.resolve('db/migrations');
+  migrations: {
+    directory: path.resolve('db/migrations')
   }
 });
