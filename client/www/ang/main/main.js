042a78332d016760d1f90b783907d733e604d75f
Benjamin Baum <bjb253@cornell.edu>
2016-03-10 22:54:11 -0600

    (feat) query for fb friends connected to app on main view

--- client/www/ang/main/main.js
+++ client/www/ang/main/main.js
@@ -1,7 +1,24 @@
 angular.module('app.main', [])
 
-.controller('mainCtrl', function($scope, $state, $ionicHistory) {
+.controller('mainCtrl', function($scope, $state, $ionicHistory, Facebook, store) {
+  
+  $scope.friends = [];
+
   $scope.account = function () {
     $ionicHistory.goBack();
   }
+
+  $scope.getFriends = function () {
+    Facebook.api('/me/friends?access_token=' + store.get('fb_access_token'), function (response) {
+      //TODO: FB authentication expired, handle this error
+      if (response.error) {
+
+      } else {
+        $scope.friends = response;
+      }
+    });
+  };
+
+  $scope.getFriends()
+
 });
