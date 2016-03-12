15d27b20fc9f75a15d09c4196d670a2e0a35908f
Benjamin Baum <bjb253@cornell.edu>
2016-03-11 19:27:57 -0600

    (chore) create findOrCreateHelper

--- db/helpers.js
+++ db/helpers.js
@@ -1,3 +1,25 @@
 var db = require('./db.js');
 var models = require('./models.js');
 var Promise = require('bluebird');
+
+module.exports.findOrCreate = function (Model, attributes) {
+
+  return new Promise (function (resolve, reject) {
+    Model.forge(attributes).fetch()
+    .then(function (model) {
+      if (!model) {
+        model = new Model(attributes);
+      }
+      model.save()
+      .then(function () {
+        resolve(model);
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+    })
+    .catch(function (error) {
+      reject(error);
+    });
+  });
+};
