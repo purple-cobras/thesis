0af95b940a3aca822a5bd2494c3d32176fdb8394
Benjamin Baum <bjb253@cornell.edu>
2016-03-11 19:03:14 -0600

    (chore) add cors middleware (needed for testing on ionic serve)

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var db = require(path.resolve('db/db'));
+var cors = require('cors');
 var app = express();
 var server= require('http').Server(app);
 
@@ -10,6 +11,7 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var io = require('./sockets.js');
 
+app.use(cors());
 app.use(bodyParser());
 app.use(morgan('dev'));
 
