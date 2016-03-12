66cac7991edd597199738af14e2a40a615c99604
Benjamin Baum <bjb253@cornell.edu>
2016-03-12 10:14:38 -0600

    (chore) use environment variable for heroku db, if available

--- db/schema.js
+++ db/schema.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 var knex = Knex({
 	client: 'postgres',
-	connection: {
+	connection: process.env.DATABASE_URL || {
 		host: 'localhost',
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
