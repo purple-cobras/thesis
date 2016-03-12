69dd08a8b75b8954964cd751096f0ec69b0110a7
Benjamin Baum <bjb253@cornell.edu>
2016-03-12 14:00:08 -0600

    (feat) display invitations on main page and add accept/decline buttons

--- client/www/ang/main/main.js
+++ client/www/ang/main/main.js
@@ -1,9 +1,53 @@
 angular.module('app.main', [])
 
-.controller('mainCtrl', function($scope, $state, $ionicHistory, Facebook, store, $rootScope) {
+.controller('mainCtrl', function($scope, $state, $ionicHistory, Facebook, store, $rootScope, $http) {
+
+
+  $scope.invitations = [{chicken: true}];
 
   $scope.account = function () {
     $ionicHistory.goBack();
+  };
+
+  $scope.getInvitations = function () {
+    $http({
+      url: Config.api + '/invitations',
+      method: 'get'
+    })
+    .then(function (response) {
+      $scope.invitations = response.data.invitations;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  };
+
+  $scope.accept = function (invitation) {
+    $http({
+      url: Config.api + '/invitations/accept',
+      method: 'post',
+      data: {invitation: invitation}
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  };
+
+  $scope.decline = function (invitation) {
+    $http({
+      url: Config.api + '/invitations/decline',
+      method: 'post',
+      data: {invitation: invitation}
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
   };
 
   $scope.getFriends = function () {
@@ -12,7 +56,6 @@ angular.module('app.main', [])
       if (response.error) {
 
       } else {
-        console.log(response);
         $rootScope.friends = response.data;
       }
     });
