e841afbb9aa521b6b9349b488dd81499e39ec776
Benjamin Baum <bjb253@cornell.edu>
2016-03-14 18:36:16 -0500

    (fix) correct behavior for game back button

--- client/www/ang/game/game.js
+++ client/www/ang/game/game.js
@@ -3,7 +3,12 @@ angular.module('app.game', [])
 .controller('gameCtrl', function($scope, $ionicHistory, $http) {
 
   $scope.back = function () {
-    $ionicHistory.goBack(-2);
+    var history = $ionicHistory.viewHistory();
+    if (history.currentView.backViewId !== "ion3") {
+      $ionicHistory.goBack(-2);
+    } else {
+      $ionicHistory.goBack();
+    }
   };
 
 });
