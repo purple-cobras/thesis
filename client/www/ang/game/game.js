8cd0433148ab6239d61e7e6e801a6bb80d52b92e
Benjamin Baum <bjb253@cornell.edu>
2016-03-14 12:55:49 -0500

    (feat) set up initial pass at $scope variables to represent game state

--- client/www/ang/game/game.js
+++ client/www/ang/game/game.js
@@ -1,8 +1,43 @@
 angular.module('app.game', [])
 
 .controller('gameCtrl', function($scope, $ionicHistory) {
-  
+
+  $scope.submitting = false;
+
+  $scope.response = '';
+
+  $scope.topic = '';
+
+  $scope.myTopic = '';
+
+  $scope.game = {
+
+    //Array of objects, with id, name, guessed, and score
+    players: [],
+
+    //Object containing all pertinent info for the round
+    round: {
+
+      reader_id: undefined
+
+      //All answers in, ready to start guessing
+      ready: false,
+
+      //Array of objects, with id, text, guessed, and user_id
+      responses: [
+
+      ]
+    }
+  };
+
+  // The guess a user is going to make
+  $scope.guess = {
+    user: undefined,
+    response: undefined
+  };
+
   $scope.back = function () {
     $ionicHistory.goBack(-2);
   };
+
 });
