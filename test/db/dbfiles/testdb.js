dc2a93e63926f7ae89bd7718847184aa146a027e
Daniel <ramossanchezd@gmail.com>
2016-03-15 19:33:30 -0700

    (chore) Updating database test files

--- test/db/dbfiles/testdb.js
+++ test/db/dbfiles/testdb.js
@@ -2,22 +2,50 @@ var Knex = require('knex');
 var Bookshelf = require('bookshelf');
 var env = require('node-env-file');
 var path = require('path');
+var inputs = require('./schema').reverse();
 
-if (process.env.NODE_ENV !== 'production' && !process.env.CIRCLECI) {
-  env(__dirname + '../../../../.env');
+if (process.env.NODE_ENV !== 'production') {
+  env(path.resolve('.env'));
 }
 
 var knex = Knex({
   client: 'postgres',
-  connection: {
+  connection: process.env.DATABASE_URL || {
     host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'thesistest',
+    database: 'thesis',
     charset: 'utf8'
   }
 });
 
 var db = Bookshelf(knex);
+
+var runInput = function (inputs) {
+  if (inputs.length) {
+    var command = inputs.pop();
+    if (typeof command === 'function') {
+      command()
+      .then(function () {
+        runInput(inputs);
+      })
+      .catch(function (error) {
+        console.log('Inputs Error:',error);
+      });
+    } else {
+      runInput(inputs);
+    }
+  } else {
+    knex.migrate.latest({directory: path.resolve('db/migrations')})
+    .then(function () {
+      console.log('any/all pending migrations complete');
+    })
+    .catch(function (error) {
+      console.log('migration error');
+    });
+  }
+};
+
+runInput(inputs);
 
 module.exports = db;
