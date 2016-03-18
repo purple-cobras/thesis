7e4ace6e4763d19547d1230a9937e10c54e4e644
Benjamin Baum <bjb253@cornell.edu>
2016-03-17 22:04:28 -0500

    (fix) fix keyboard issues

--- client/www/ang/app.js
+++ client/www/ang/app.js
@@ -30,6 +30,8 @@ angular.module('app',
 
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
     }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
