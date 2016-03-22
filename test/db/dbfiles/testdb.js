67c812c5d6c2a98c973de938c2b859b0297833d4
Daniel <ramossanchezd@gmail.com>
2016-03-22 16:12:20 -0700

    (fix) Adds testdb back in

---
+++ test/db/dbfiles/testdb.js
@@ -0,0 +1,23 @@
+var Knex = require('knex');
+var Bookshelf = require('bookshelf');
+var env = require('node-env-file');
+var path = require('path');
+
+if (process.env.NODE_ENV !== 'production' && !process.env.CIRCLECI) {
+  env(__dirname + '../../../../.env');
+}
+
+var knex = Knex({
+  client: 'postgres',
+  connection: {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'thesistest',
+    charset: 'utf8'
+  }
+});
+
+var db = Bookshelf(knex);
+
+module.exports = db;
