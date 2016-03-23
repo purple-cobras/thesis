2ddc3de00e9519249e52d087a8aef5dd201a65d1
Daniel <ramossanchezd@gmail.com>
2016-03-22 17:33:07 -0700

    (test) only running karma start in npm test

--- test/backend-tests/dbSpec.js
+++ test/backend-tests/dbSpec.js
@@ -7,20 +7,20 @@ if (process.env.NODE_ENV !== 'production' && !process.env.CIRCLECI) {
   env(__dirname + '../../../.env');
 }
 
-// describe('it should have a connection to the database', function () {
-//   it('should use a postgres client', function () {
-//       expect(db.knex.client.config.client).to.equal('postgres');
-//   });
-//   it('should have a connection to the databse', function () {
-//       expect(db.knex.client.config.connection).to.exist
-//   });
-//   it('should accept a user', function () {
-//       expect(db.knex.client.config.connection.user).to.equal('');
-//   });
-//   it('should accept a password', function () {
-//       expect(db.knex.client.config.connection.password).to.equal('');
-//   });
-//   it('should connect to the thesis database', function () {
-//       expect(db.knex.client.config.connection.database).to.equal('thesis');
-//   });
-// });
+describe('it should have a connection to the database', function () {
+  it('should use a postgres client', function () {
+      expect(db.knex.client.config.client).to.equal('postgres');
+  });
+  it('should have a connection to the databse', function () {
+      expect(db.knex.client.config.connection).to.exist
+  });
+  it('should accept a user', function () {
+      expect(db.knex.client.config.connection.user).to.equal('');
+  });
+  it('should accept a password', function () {
+      expect(db.knex.client.config.connection.password).to.equal('');
+  });
+  it('should connect to the thesis database', function () {
+      expect(db.knex.client.config.connection.database).to.equal('thesis');
+  });
+});
