dc6c8d55160db245d7a6762992dba76b0029e211
Benjamin Baum <bjb253@cornell.edu>
2016-03-24 14:10:28 -0500

    (chore) create basic framework for sending alchemy requests to grab the data we need

---
+++ server/alchemy.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var request = require('request');
+var env = require('node-env-file');
+
+if (process.env.NODE_ENV !== 'production') {
+  env(path.resolve('.env'));
+}
+
+var features = ['doc-sentiment', 'doc-emotion', 'taxonomy'];
+
+var api_key = process.env.ALCHEMY_API_KEY;
+var base_url = 'http://gateway-a.watsonplatform.net/calls/text/TextGetCombinedData';
+
+var alchemy = function (text, options) {
+  url = '';
+  url += '?apikey=' + api_key;
+  url += '&extract='
+  features.forEach(function(feature, index) {
+    var comma = index === features.length - 1 ? '' : ',';
+    url += encodeURIComponent(feature) + comma;
+  });
+  url += '&outputMode=json';
+  url += '&text=' + encodeURIComponent(text);
+  url = base_url + url;
+  request(url, function(error, response, body) {
+    if (error) {
+      if (options.error) {
+        options.error(error);
+      }
+    } else {
+      if (options.success) {
+        options.success(response, body);
+      }
+    }
+  })
+};
+
+module.exports = alchemy;
