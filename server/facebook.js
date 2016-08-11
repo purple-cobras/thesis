6c0b877a40b63f285151716a548dfdd4ea7bdcd1
Tom Coughlin <tcoughlin3@gmail.com>
2016-08-11 08:51:46 -0500

    (refactor) rearranges function declarations

--- server/facebook.js
+++ server/facebook.js
@@ -11,29 +11,25 @@ var client_secret = process.env.AUTH0_MGMT_API_SECRET;
 
 function getFBAccessToken(userId) {
 
-  return authenticateAuth0MgmtClient()
-    .then(extractAccessToken)
+  return authenticateBackendClient()
+    .then(extractAuth0Token)
     .then(getFBProfile)
     .then(extractFBToken)
     .catch(handleError);
 
-  function handleError(error) {
-    console.log('fb.js error', error)
-  }
-
-  function extractFBToken(body) {
-    var identities = parseReply(body, 'identities');
-    return identities[0].access_token;
-  }
-
-  function extractAccessToken(body) {
-    return parseReply(body, 'access_token');
-  }
-
-  function parseReply(str, prop) {
-    var obj = JSON.parse(str);
-    if (prop !== undefined) return obj[prop];
-    return obj;
+  function authenticateBackendClient() {
+    return new Promise(function(resolve, reject) {
+      var options = {
+        method: 'POST',
+        url: 'https://purplecobras.auth0.com/oauth/token',
+        headers: { 'content-type': 'application/json' },
+        body: '{"client_id":"' + client_id + '","client_secret":"' + client_secret + '","audience":"https://purplecobras.auth0.com/api/v2/","grant_type":"client_credentials"}'
+      };
+      request(options, function (error, response, body) {
+        if (error) reject(error);
+        resolve(body);
+      });
+    });
   }
 
   function getFBProfile(auth0MgmtToken) {
@@ -53,19 +49,23 @@ function getFBAccessToken(userId) {
     });
   }
 
-  function authenticateAuth0MgmtClient() {
-    return new Promise(function(resolve, reject) {
-      var options = {
-        method: 'POST',
-        url: 'https://purplecobras.auth0.com/oauth/token',
-        headers: { 'content-type': 'application/json' },
-        body: '{"client_id":"' + client_id + '","client_secret":"' + client_secret + '","audience":"https://purplecobras.auth0.com/api/v2/","grant_type":"client_credentials"}'
-      };
-      request(options, function (error, response, body) {
-        if (error) reject(error);
-        resolve(body);
-      });
-    });
+  function extractAuth0Token(body) {
+    return parseReply(body, 'access_token');
+  }
+
+  function extractFBToken(body) {
+    var identities = parseReply(body, 'identities');
+    return identities[0].access_token;
+  }
+
+  function parseReply(str, prop) {
+    var obj = JSON.parse(str);
+    if (prop !== undefined) return obj[prop];
+    return obj;
+  }
+
+  function handleError(error) {
+    console.log('fb.js error', error)
   }
 }
 
